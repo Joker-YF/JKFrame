7c3419844f9d753840be0d06efc8988a65cd6bfc
Joker <739554159@qq.com>
2022-05-08 14:44:22 +0800

    修复Addressable打包失败
    由于GameRoot using UnityEditor;

--- JKFrame/Scripts/1.Base/GameRoot.cs
+++ JKFrame/Scripts/1.Base/GameRoot.cs
@@ -1,9 +1,12 @@
-﻿using UnityEditor;
-using UnityEngine;
-
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 namespace JKFrame
 {
+#if UNITY_EDITOR
     [InitializeOnLoad]
+#endif
     public class GameRoot : SingletonMono<GameRoot>
     {
         /// <summary>
