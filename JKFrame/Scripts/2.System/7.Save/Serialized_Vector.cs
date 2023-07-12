abaf67143a71ea206a29227db196bb0c156dfd51
Joker <739554159@qq.com>
2023-07-12 11:15:07 +0800

    增加一些可序列化Vector的转换函数

--- JKFrame/Scripts/2.System/7.Save/Serialized_Vector.cs
+++ JKFrame/Scripts/2.System/7.Save/Serialized_Vector.cs
@@ -39,47 +39,44 @@ public struct Serialized_Vector2
 
 public static class SerializedVectorExtensions
 {
-    /// <summary>
-    /// SV3תUnity V3
-    /// </summary>
+
     public static Vector3 ConverToVector3(this Serialized_Vector3 sv3)
     {
         return new Vector3(sv3.x, sv3.y, sv3.z);
     }
-    /// <summary>
-    /// V3תSV3
-    /// </summary>
+
     public static Serialized_Vector3 ConverToSVector3(this Vector3 v3)
     {
         return new Serialized_Vector3(v3.x, v3.y, v3.z);
     }
 
-    /// <summary>
-    /// SV2תV2
-    /// </summary>
+
+    public static Vector3Int ConverToVector3Int(this Serialized_Vector3 sv3)
+    {
+        return new Vector3Int((int)sv3.x, (int)sv3.y, (int)sv3.z);
+    }
+
+    public static Serialized_Vector3 ConverToSVector3Int(this Vector3Int v3)
+    {
+        return new Serialized_Vector3(v3.x, v3.y, v3.z);
+    }
+
+
     public static Vector2 ConverToSVector2(this Serialized_Vector2 sv2)
     {
         return new Vector2(sv2.x, sv2.y);
     }
-    /// <summary>
-    /// SV2תV2Int
-    /// </summary>
+
     public static Vector2Int ConverToVector2Int(this Serialized_Vector2 sv2)
     {
         return new Vector2Int((int)sv2.x, (int)sv2.y);
     }
 
-    /// <summary>
-    /// V2תSV2
-    /// </summary>
     public static Serialized_Vector2 ConverToSVector2(this Vector2 v2)
     {
         return new Serialized_Vector2(v2.x, v2.y);
     }
 
-    /// <summary>
-    /// V2InitתSV2
-    /// </summary>
     public static Serialized_Vector2 ConverToSVector2(this Vector2Int v2)
     {
         return new Serialized_Vector2(v2.x, v2.y);
