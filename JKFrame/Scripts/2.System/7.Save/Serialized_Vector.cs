f1909b365f155ae66914fcd14eb9e1cb32994a0f
Joker <739554159@qq.com>
2023-07-12 11:29:47 +0800

    添加可序列化Vector的隐式转换

--- JKFrame/Scripts/2.System/7.Save/Serialized_Vector.cs
+++ JKFrame/Scripts/2.System/7.Save/Serialized_Vector.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-using System;
 [Serializable]
 public struct Serialized_Vector3
 {
@@ -17,6 +15,23 @@ public struct Serialized_Vector3
     public override string ToString()
     {
         return $"({x},{y},{z})";
+    }
+
+    public static implicit operator Serialized_Vector3(Vector3 vector3)
+    {
+        return new Serialized_Vector3(vector3.x, vector3.y, vector3.z);
+    }
+    public static implicit operator Vector3(Serialized_Vector3 vector3)
+    {
+        return new Vector3(vector3.x, vector3.y, vector3.z);
+    }
+    public static implicit operator Serialized_Vector3(Vector3Int vector3)
+    {
+        return new Serialized_Vector3(vector3.x, vector3.y, vector3.z);
+    }
+    public static implicit operator Vector3Int(Serialized_Vector3 vector3)
+    {
+        return new Vector3Int((int)vector3.x, (int)vector3.y, (int)vector3.z);
     }
 }
 
@@ -34,6 +49,23 @@ public struct Serialized_Vector2
     {
         return $"({x},{y})";
     }
+
+    public static implicit operator Serialized_Vector2(Vector2 vector2)
+    {
+        return new Serialized_Vector2(vector2.x, vector2.y);
+    }
+    public static implicit operator Vector2(Serialized_Vector2 vector2)
+    {
+        return new Vector3(vector2.x, vector2.y);
+    }
+    public static implicit operator Serialized_Vector2(Vector2Int vector2)
+    {
+        return new Serialized_Vector2(vector2.x, vector2.y);
+    }
+    public static implicit operator Vector2Int(Serialized_Vector2 vector2)
+    {
+        return new Vector2Int((int)vector2.x, (int)vector2.y);
+    }
 }
 
 
@@ -50,7 +82,6 @@ public static class SerializedVectorExtensions
         return new Serialized_Vector3(v3.x, v3.y, v3.z);
     }
 
-
     public static Vector3Int ConverToVector3Int(this Serialized_Vector3 sv3)
     {
         return new Vector3Int((int)sv3.x, (int)sv3.y, (int)sv3.z);
@@ -60,7 +91,6 @@ public static class SerializedVectorExtensions
     {
         return new Serialized_Vector3(v3.x, v3.y, v3.z);
     }
-
 
     public static Vector2 ConverToSVector2(this Serialized_Vector2 sv2)
     {
@@ -81,6 +111,4 @@ public static class SerializedVectorExtensions
     {
         return new Serialized_Vector2(v2.x, v2.y);
     }
-
-
 }
