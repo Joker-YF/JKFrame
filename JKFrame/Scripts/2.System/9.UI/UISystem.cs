d627ae5f95d939c1508fd9b29b189aba8de017f5
Joker <739554159@qq.com>
2023-12-17 16:47:05 +0800

    修复拼写错误

--- JKFrame/Scripts/2.System/9.UI/UISystem.cs
+++ JKFrame/Scripts/2.System/9.UI/UISystem.cs
@@ -527,7 +527,7 @@ namespace JKFrame
 #if ENABLE_LEGACY_INPUT_MANAGER
             return CheckPositionOnUI(Input.mousePosition);
 #else
-            return CheckPositoinOnUI(UnityEngine.InputSystem.Mouse.current.position.ReadValue());
+            return CheckPositionOnUI(UnityEngine.InputSystem.Mouse.current.position.ReadValue());
 #endif
 
         }
