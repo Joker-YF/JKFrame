178f839656c9b613fcbb6a7c894b264ba36a9466
Joker <739554159@qq.com>
2024-01-16 14:28:57 +0800

    添加运行时显示日志的能力
    基本上就是简版Console，支持Debug.Log以及JKLog.Log相关API，将框架目录下Prefabs/RuntimeScreenLogger拽入JKFrame根游戏物体即可，RuntimeScreenLogger有字体、字体大小、尺寸等相关设置

---
+++ JKFrame/Scripts/2.System/8.LogSystem/RuntimeScreeLogger.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeScreeLogger : MonoBehaviour
+{
+    private struct LogMessage
+    {
+        public string time;
+        public string log;
+        public string stackTrace;
+        public UnityEngine.LogType logType;
+    }
+    private List<LogMessage> logs = new List<LogMessage>();
+    private GUIStyle mainContainerStyle;
+    private GUIStyle logStyle;
+    private GUIStyle warningStyle;
+    private GUIStyle errorStyle;
+    private GUIStyle logSelectStyle;
+    private GUIStyle buttonStyle;
+    private GUIStyle buttonSelectStyle;
+    private GUIStyle stackTraceLableStyle;
+    private GUIStyle lineStyle;
+    public Font font;
+    public int fontSize = 20;
+    public bool show;
+    public float margin = 20;
+    public float width = 576;
+    public float height = 400;
+    private void Awake()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        isFirstEnterOnGUI = true;
+        // 创建初始样式
+        Texture2D bgTex = new Texture2D(1, 1);
+        Color bgColor = new Color(0, 0, 0, 0.5f);
+        bgTex.SetPixel(0, 0, bgColor);
+        bgTex.Apply();
+        mainContainerStyle = new GUIStyle();
+        mainContainerStyle.normal.background = bgTex;
+        mainContainerStyle.font = font;
+        mainContainerStyle.border = new RectOffset(5, 5, 5, 5);
+
+        Texture2D lineTex = new Texture2D(1, 1);
+        Color lineColor = new Color(0, 0, 0, 0.75f);
+        lineTex.SetPixel(0, 0, bgColor);
+        lineTex.Apply();
+        lineStyle = new GUIStyle();
+        lineStyle.normal.background = lineTex;
+
+
+        logStyle = new GUIStyle();
+        logStyle.font = font;
+        logStyle.fontSize = fontSize;
+        logStyle.normal.textColor = Color.white;
+        logStyle.margin = new RectOffset(10, 5, 5, 5);
+
+        warningStyle = new GUIStyle(logStyle);
+        warningStyle.normal.textColor = Color.yellow;
+
+        errorStyle = new GUIStyle(logStyle);
+        errorStyle.normal.textColor = Color.red;
+
+        logSelectStyle = new GUIStyle();
+        logSelectStyle.font = font;
+        logSelectStyle.fontSize = fontSize;
+        logSelectStyle.normal.textColor = Color.gray;
+        logSelectStyle.margin = new RectOffset(5, 5, 5, 5);
+
+        stackTraceLableStyle = new GUIStyle(logStyle);
+
+
+        // 初步绘制需要的数据
+        width = Screen.width * 0.3f;
+        height = Screen.height * 0.3f + stackTraceHeight;
+
+        Application.logMessageReceived += HandleLog;
+    }
+    private void OnDestroy()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string condition, string stackTrace, UnityEngine.LogType type)
+    {
+        condition = ProcessingLogString(condition);
+        stackTrace = ProcessingStackTraceString(stackTrace);
+        LogMessage logMessage = new LogMessage()
+        {
+            log = condition,
+            stackTrace = stackTrace,
+            logType = type,
+            time = DateTime.Now.ToString("HH:mm:ss")
+        };
+        logs.Add(logMessage);
+
+    }
+
+    private string ProcessingLogString(string logString)
+    {
+        // 去除最后一行可能的换行符
+        if (logString[logString.Length - 1] == '\n')
+        {
+            return logString.Remove(logString.Length - 1, 1);
+        }
+        return logString;
+    }
+    private string ProcessingStackTraceString(string stackTrace)
+    {
+        int stackTrackRemoveIndex = -1;
+        if (stackTrace.Contains("JKFrame.JKLog")) // 去除4行JKLog带来的堆栈信息
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                stackTrackRemoveIndex = stackTrace.IndexOf("\n", stackTrackRemoveIndex + 1);
+            }
+        }
+        else // 常规Debug.log 去除顶行堆栈信息
+        {
+            stackTrackRemoveIndex = stackTrace.IndexOf("\n");
+        }
+        stackTrace = stackTrace.Remove(0, stackTrackRemoveIndex + 1);
+        return stackTrace;
+    }
+
+    private Vector2 stackTraceScrollPosition;
+    private Vector2 logScrollPosition;
+
+    private float posX;
+    private float posY;
+    private int selectIndex = -1;
+    private float stackTraceHeight = 200;
+    private bool showLog = true;
+    private bool showWarning = true;
+    private bool showError = true;
+
+    private bool isFirstEnterOnGUI = true;
+    private void OnGUI()
+    {
+        if (isFirstEnterOnGUI)
+        {
+            isFirstEnterOnGUI = false;
+            buttonStyle = new GUIStyle(GUI.skin.button);
+            buttonStyle.normal.textColor = Color.gray;
+            buttonStyle.hover.textColor = Color.gray;
+
+            buttonSelectStyle = new GUIStyle(GUI.skin.button);
+            buttonSelectStyle.normal.textColor = Color.white;
+            buttonSelectStyle.hover.textColor = Color.white;
+        }
+
+
+        posX = margin;
+        posY = Screen.height - height - margin;
+        if (show)
+        {
+            GUILayout.BeginArea(new Rect(posX, posY, width, height), mainContainerStyle);
+            // 顶部菜单
+            GUILayout.BeginHorizontal(GUILayout.Height(30));
+            if (GUILayout.Button("Clear"))
+            {
+                logs.Clear();
+            }
+            if (GUILayout.Button("Log", showLog ? buttonSelectStyle : buttonStyle))
+            {
+                showLog = !showLog;
+            }
+            if (GUILayout.Button("Warning", showWarning ? buttonSelectStyle : buttonStyle))
+            {
+                showWarning = !showWarning;
+            }
+            if (GUILayout.Button("Error", showError ? buttonSelectStyle : buttonStyle))
+            {
+                showError = !showError;
+            }
+            if (GUILayout.Button("<<<Hide"))
+            {
+                selectIndex = -1;
+                show = false;
+            }
+
+            GUILayout.EndHorizontal();
+            // 滚动区域
+            logScrollPosition = GUILayout.BeginScrollView(logScrollPosition, GUILayout.Width(width), GUILayout.Height(height - 30 - stackTraceHeight));
+            for (int i = 0; i < logs.Count; i++)
+            {
+                LogMessage logMessage = logs[i];
+
+                if (logMessage.logType == UnityEngine.LogType.Log && showLog == false) continue;
+                if (logMessage.logType == UnityEngine.LogType.Warning && showWarning == false) continue;
+                if (logMessage.logType == UnityEngine.LogType.Error && showError == false) continue;
+
+                GUIStyle normalLogStype;
+                switch (logMessage.logType)
+                {
+                    case UnityEngine.LogType.Error:
+                        normalLogStype = errorStyle;
+                        break;
+                    case UnityEngine.LogType.Warning:
+                        normalLogStype = warningStyle;
+                        break;
+                    default:
+                        normalLogStype = logStyle;
+                        break;
+                }
+
+                string timeAndLog = $"[{logMessage.time}]:{logMessage.log}";
+                if (i != selectIndex)
+                {
+                    if (GUILayout.Button(timeAndLog, normalLogStype))
+                    {
+                        selectIndex = i;
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button(timeAndLog, logSelectStyle))
+                    {
+                        selectIndex = -1;
+                    }
+                }
+            }
+            GUILayout.EndScrollView();
+            // 堆栈信息
+            GUILayout.Box("", lineStyle, GUILayout.Width(width), GUILayout.Height(3));
+            stackTraceScrollPosition = GUILayout.BeginScrollView(stackTraceScrollPosition, GUILayout.Width(width), GUILayout.Height(stackTraceHeight - 10));
+            if (selectIndex != -1)
+            {
+                GUILayout.Label(logs[selectIndex].log, stackTraceLableStyle);
+                GUILayout.Label(logs[selectIndex].logType.ToString(), stackTraceLableStyle);
+                GUILayout.Label(logs[selectIndex].stackTrace, stackTraceLableStyle);
+            }
+            GUILayout.EndScrollView();
+            GUILayout.EndArea();
+        }
+        else
+        {
+            GUILayout.BeginArea(new Rect(margin, Screen.height - 60 - margin, 45, 45));
+            if (GUILayout.Button("Log", showLog ? buttonSelectStyle : buttonStyle))
+            {
+                show = true;
+            }
+            GUILayout.EndArea();
+        }
+    }
+
+}
